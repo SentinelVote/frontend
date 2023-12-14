99fa9525f08bd412be77982ed45b0987f70d9327
Elroy Chua <elroymx@gmail.com>
2023-12-14 11:27:47 +0800

    feat(pages): add singpass component in login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import singpassQrPng from "../../public/singpass_qr.png";
+enum LoginType {
+  email = "email",
+  singpass = "singpass",
+}
+
 export default function Home() {
+  const [loginType, setLoginType] = useState(LoginType.email);
   return (
     <>
       <main
@@ -20,33 +30,82 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-col flex-1 justify-center">
-            <div className="flex flex-col gap-4 w-[350px] h-[450px] bg-white rounded-md px-8 py-10 self-center">
-              <h1 className="font-medium text-xl text-black">Login Details</h1>
-              <div className="flex flex-col gap-2 pt-10 text-gray-500">
-                <input
-                  className="border-2 border-slate-900 rounded-lg px-4 py-2"
-                  type="text"
-                  name="email"
-                  id="email"
-                />
-                <input
-                  className="border-2 border-slate-900 rounded-lg px-4 py-2"
-                  type="password"
-                  name="password"
-                  id="password"
-                />
-                <p className="self-end">Forgot password?</p>
+            <div className="flex flex-col gap-4 w-[400px] h-[450px] bg-white rounded-md p-8 self-center justify-between">
+              <div className="flex justify-between">
+                <h1
+                  className={`font-medium text-xl ${
+                    loginType === LoginType.email
+                      ? "text-black"
+                      : "text-gray-400"
+                  } cursor-pointer`}
+                  onClick={() => setLoginType(LoginType.email)}
+                >
+                  Login Details
+                </h1>
+                <h1
+                  className={`font-medium text-xl ${
+                    loginType === LoginType.singpass
+                      ? "text-black"
+                      : "text-gray-400"
+                  } cursor-pointer`}
+                  onClick={() => setLoginType(LoginType.singpass)}
+                >
+                  Sign in via SingPass
+                </h1>
               </div>
-              <Link href="/pem-uploader">
-                <button
-                  type="button"
-                  className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4
+              {loginType === LoginType.email ? (
+                <>
+                  <div className="flex flex-col gap-2 pt-10 text-gray-500">
+                    <input
+                      className="border-2 border-slate-900 rounded-lg px-4 py-2"
+                      type="text"
+                      name="email"
+                      id="email"
+                    />
+                    <input
+                      className="border-2 border-slate-900 rounded-lg px-4 py-2"
+                      type="password"
+                      name="password"
+                      id="password"
+                    />
+                    <p className="self-end">Forgot password?</p>
+                  </div>
+                  <Link href="/pem-uploader">
+                    <button
+                      type="button"
+                      className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4
                  focus:ring-red-300 font-medium rounded-lg text-sm self-center px-5 py-2.5 w-full
                  me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mt-24"
-                >
-                  Log In
-                </button>
-              </Link>
+                    >
+                      Log In
+                    </button>
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <div className="flex flex-col justify-center selection:gap-2 text-gray-500">
+                    <Image
+                      className="self-center"
+                      src={singpassQrPng}
+                      alt="Singpass Qr"
+                      height={225}
+                      width={225}
+                    />
+                    <h1 className="text-black text-xl text-center pt-5 pb-2">
+                      SCAN WITH SINGPASS MOBILE APP TO LOG IN
+                    </h1>
+                    <h1 className="text-center">
+                      {`Don't have Singpass mobile? `}
+                      <Link
+                        href="https://www.singpass.gov.sg/home/ui/register/instructions"
+                        target="_blank"
+                      >
+                        <span className="text-blue-500">Find out more. </span>
+                      </Link>
+                    </h1>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
