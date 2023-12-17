2bdf5be5b8fae49c08488087a8a50c87ebf1f85f
Elroy Chua <elroymx@gmail.com>
2023-12-18 02:24:57 +0800

    chore: update knex seed files
    Co-authored-by: Baraq Kamsani <mkamsani@users.noreply.github.com>

--- seeds/seed_users.js
+++ seeds/seed_users.js
@@ -3,25 +3,97 @@ const bcrypt = require("bcrypt");
 exports.seed = function (knex) {
   const saltRounds = 10;
   return knex("users")
-    .del()
+    .del() // TODO remove this line before final submission.
     .then(function () {
       return knex("users").insert([
         {
+          userEmail: "admin@email.com",
+          hashedPassword: bcrypt.hashSync("password", saltRounds),
+          publicKey: "",
+          firstName: "Helios",
+          lastName: "Management",
+          role: "admin",
+        },
+        {
           userEmail: "user1@email.com",
-          hashedPassword: bcrypt.hashSync("password1", saltRounds),
-          publicKey: "publickey1",
-          firstName: "John",
-          lastName: "Doe",
+          hashedPassword: bcrypt.hashSync("password", saltRounds),
+          publicKey: "",
+          firstName: "Alice",
+          lastName: "Wonderland",
           role: "voter",
         },
         {
           userEmail: "user2@email.com",
-          hashedPassword: bcrypt.hashSync("password2", saltRounds),
-          publicKey: "publickey2",
-          firstName: "Jane",
-          lastName: "Doe",
-          role: "admin",
+          hashedPassword: bcrypt.hashSync("password", saltRounds),
+          publicKey: "",
+          firstName: "Bob",
+          lastName: "Builder",
+          role: "voter",
         },
+        // {
+        //   userEmail: "user3@email.com",
+        //   hashedPassword: bcrypt.hashSync("password", saltRounds),
+        //   publicKey: "",
+        //   firstName: "Charlie",
+        //   lastName: "Sheen",
+        //   role: "voter",
+        // },
+        // {
+        //   userEmail: "user4@email.com",
+        //   hashedPassword: bcrypt.hashSync("password", saltRounds),
+        //   publicKey: "",
+        //   firstName: "Dave",
+        //   lastName: "Doe",
+        //   role: "voter",
+        // },
+        // {
+        //   userEmail: "user5@email.com",
+        //   hashedPassword: bcrypt.hashSync("password", saltRounds),
+        //   publicKey: "",
+        //   firstName: "Elliot",
+        //   lastName: "Doe",
+        //   role: "voter",
+        // },
+        // {
+        //   userEmail: "user6@email.com",
+        //   hashedPassword: bcrypt.hashSync("password", saltRounds),
+        //   publicKey: "",
+        //   firstName: "Frank",
+        //   lastName: "Doe",
+        //   role: "voter",
+        // },
+        // {
+        //   userEmail: "user7@email.com",
+        //   hashedPassword: bcrypt.hashSync("password", saltRounds),
+        //   publicKey: "",
+        //   firstName: "Sarah",
+        //   lastName: "Chua",
+        //   role: "voter",
+        // },
+        // {
+        //   userEmail: "user8@email.com",
+        //   hashedPassword: bcrypt.hashSync("password", saltRounds),
+        //   publicKey: "",
+        //   firstName: "Jane",
+        //   lastName: "Doe",
+        //   role: "voter",
+        // },
+        // {
+        //   userEmail: "user9@email.com",
+        //   hashedPassword: bcrypt.hashSync("password", saltRounds),
+        //   publicKey: "",
+        //   firstName: "John",
+        //   lastName: "Doe",
+        //   role: "voter",
+        // },
+        // {
+        //   userEmail: "user10@email.com",
+        //   hashedPassword: bcrypt.hashSync("password", saltRounds),
+        //   publicKey: "",
+        //   firstName: "Eric",
+        //   lastName: "Dawson",
+        //   role: "voter",
+        // },
       ]);
     });
 };
