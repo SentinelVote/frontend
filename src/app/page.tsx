6475e972b3f3defb3df0bdef6790ff1d5196ae7d
Elroy Chua <elroymx@gmail.com>
2023-12-18 02:26:41 +0800

    feat(pages): routing logic; refine styling
    Co-authored-by: Baraq Kamsani <mkamsani@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,32 @@ export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loginError, setLoginError] = useState("");
+  const getUsers = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/api/users");
+      const data = await response.json();
+      console.log({ data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  getUsers();
+  const checkAllUsersHaveKey = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3001/api/check-voter-keys"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  };
 
   const handleLogin = async (e: any) => {
     e.preventDefault();
@@ -26,9 +52,35 @@ export default function Home() {
         body: JSON.stringify({ email, password }),
       });
       const data = await response.json();
+
       if (data.success) {
         console.log("Login successful");
-        window.location.href = "/pem-uploader";
+        console.log(data.user);
+        //Add cookie to browser
+        document.cookie = `user_email=${data.user.userEmail}`;
+        document.cookie = `role=${data.user.role}`;
+        console.log(document.cookie);
+
+        // Check if every voter in the database has a public key:
+
+        if (data.role === "admin") {
+          // Handle admin redirection.
+          window.location.href = "/"; // TODO: change to admin page.
+        } else {
+          // Handle voter redirection.
+          const allUsersHaveKey = await checkAllUsersHaveKey();
+          console.log("allUsersHaveKey", allUsersHaveKey);
+          if (allUsersHaveKey) {
+            // Election day.
+            window.location.href = "/pem-uploader";
+          } else if (data.user.publicKey === "") {
+            // This user has not generated their key yet.
+            window.location.href = "/pem-generate";
+          } else {
+            // This user has generated their key, but not everyone has.
+            window.location.href = "/pending-election";
+          }
+        }
       } else {
         setLoginError("Invalid email or password");
       }
@@ -37,17 +89,18 @@ export default function Home() {
       setLoginError("An error occurred during login");
     }
   };
+
   return (
     <>
       <main
-        className="flex flex-col items-center justify-between p-24 bg-gradient-to-r from-slate-900 to-slate-700 text-slate-900"
+        className="flex flex-col items-center justify-between p-8 lg:p-24 bg-gradient-to-r from-slate-900 to-slate-700 text-slate-900"
         style={{
           minHeight: "90vh",
           overflow: "hidden",
           justifyContent: "center",
         }}
       >
-        <div className="flex w-full text-white">
+        <div className="flex flex-col md:flex-row w-full text-white">
           <div className="flex flex-col flex-1 justify-center">
             <div className="flex flex-col">
               <h1 className="font-bold text-5xl">Enter your login details</h1>
@@ -56,11 +109,11 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col flex-1 justify-center">
-            <div className="flex flex-col gap-4 w-[400px] h-[450px] bg-white rounded-md p-8 self-center justify-between">
+          <div className="flex flex-col flex-1 justify-center mt-6 lg:mt-0">
+            <div className="flex flex-col gap-4 w-[300px] h-[350px] lg:w-[400px] lg:h-[450px] bg-white rounded-md p-8 self-center justify-between">
               <div className="flex justify-between">
                 <h1
-                  className={`font-medium text-xl ${
+                  className={`font-medium text-lg lg:text-xl ${
                     loginType === LoginType.email
                       ? "text-black"
                       : "text-gray-400"
@@ -70,7 +123,7 @@ export default function Home() {
                   Login Details
                 </h1>
                 <h1
-                  className={`font-medium text-xl ${
+                  className={`font-medium text-lg lg:text-xl ${
                     loginType === LoginType.singpass
                       ? "text-black"
                       : "text-gray-400"
@@ -108,7 +161,7 @@ export default function Home() {
                       type="submit"
                       className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4
               focus:ring-red-300 font-medium rounded-lg text-sm self-center px-5 py-2.5 w-full
-              me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mt-24"
+              me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mt-4 lg:mt-24"
                     >
                       Log In
                     </button>
