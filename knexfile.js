9b782b2d3704ec55043d3b268d8b18e559867a29
Elroy Chua <elroymx@gmail.com>
2023-12-19 12:54:34 +0800

    chore: update knex migration files to align with new requirements
    Co-authored-by: Baraq Kamsani <mkamsani@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,8 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
   development: {
     client: "sqlite3",
@@ -5,5 +10,37 @@ module.exports = {
       filename: "./dev.sqlite3",
     },
     useNullAsDefault: true,
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
   },
 };
--- migrations/20231215120317_create_users.js
+++ migrations/20231215120317_create_users.js
@@ -1,13 +1,23 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("users", (table) => {
-    table.increments("id").primary();
-    table.string("userEmail").notNullable().unique();
-    table.string("hashedPassword").notNullable();
-    table.string("publicKey").notNullable();
-    table.string("firstName").notNullable();
-    table.string("lastName").notNullable();
-    table.string("role").notNullable().defaultTo("voter");
-  });
+  return knex.schema
+    .createTable("users", (table) => {
+      table.increments("id").primary();
+      table.string("userEmail").notNullable().unique();
+      table.string("hashedPassword").notNullable();
+      table.string("publicKey").notNullable();
+      table.string("firstName").notNullable();
+      table.string("lastName").notNullable();
+      table.string("region").notNullable();
+      table.string("role").notNullable().defaultTo("voter");
+    })
+    .then(
+      // This table has only one row, and one field.
+      knex.schema.createTable("foldedPublicKeys", (table) => {
+        table.string("foldedPublicKeys").defaultTo("").primary();
+      })
+    );
+
+  // table
 };
 
 exports.down = function (knex) {
