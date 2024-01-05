ab82072d777d490987654a122700977c59ac65bd
Elroy Chua <elroymx@gmail.com>
2024-01-05 11:02:51 +0800

    feat: improved styling and add styling template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,17 +94,19 @@ export default function Home() {
   return (
     <>
       <div className="text-white h-[10vh]">
-        <div className="flex justify-center md:justify-normal items-center h-full px-10 md:px-24">
+        <div className="flex justify-center md:justify-normal items-center h-full px-10 md:px-24 bg-navbar-bg">
           <Image
             src="/SentinelVote.ico"
             width={36}
             height={36}
             alt="SentinelVote Logo"
           />
-          <h1 className="text-2xl p-2 md:px-5 font-bold border-r-[3px] md:border-r-[5px] border-white">
-            SentinelVote
+          <h1 className="text-2xl p-2 md:pl-2 md:pr-5 font-semibold  ">
+            sentinelvote
           </h1>
-          <h1 className="text-2xl pl-2 md:pl-5">Login</h1>
+          <h1 className="text-lg pl-2 md:pl-5 border-l-[3px] md:border-l-[3px] border-white">
+            login
+          </h1>
         </div>
       </div>
       <main
