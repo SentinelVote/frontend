34a7910bef926675c4735972a11c1413f027a0cf
mkamsani <mkamsani@posteo.org>
2024-01-18 02:31:16 +0800

    fix: corrected assignment, code cleanup, improve doc

--- public/linkable-ring-signature/main.go
+++ public/linkable-ring-signature/main.go
@@ -3,24 +3,37 @@ package main
 import (
 	"fmt"
 	"github.com/zbohm/lirisi/client"
+	"github.com/zbohm/lirisi/ring"
 	"syscall/js"
-	_ "syscall/js"
 )
 
 var document js.Value
 
+// main is the entry point for the WebAssembly module.
+//
+// It modifies two web page elements to the value of the keys.
+// Ideally, having main() return two strings would be better,
+// but doing so is complex, less efficient, and harder to understand.
+//
+// The point here is that a client can generate a key pair in the browser.
 func main() {
 	document = js.Global().Get("document")
 
 	status, privateKey := client.GeneratePrivateKey("prime256v1", "PEM")
-	fmt.Println("client.GeneratePrivateKey(): ", status)
-	SetValue("pk", "innerHTML", string(privateKey))
+	if status != ring.Success {
+		fmt.Println(`Unable to generate a private key: `, status)
+	}
+
 	fmt.Println("Print from Go: ", string(privateKey))
+	SetValue("sk", "innerHTML", string(privateKey))
 
 	status, publicKey := client.DerivePublicKey(privateKey, "PEM")
-	fmt.Println("client.DerivePublicKey(): ", status)
-	SetValue("sk", "innerHTML", string(publicKey))
+	if status != ring.Success {
+		fmt.Println("Unable to derive a public key: ", status)
+	}
+
 	fmt.Println("Print from Go: ", string(publicKey))
+	SetValue("pk", "innerHTML", string(publicKey))
 }
 
 // SetValue sets the value of the specified element.
