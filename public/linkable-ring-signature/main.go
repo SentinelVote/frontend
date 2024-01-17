eda31ea01de446e76fadb45f348d1644b5b6f58c
mkamsani <mkamsani@posteo.org>
2024-01-18 02:00:39 +0800

    feat: client-side keygen works

--- public/linkable-ring-signature/main.go
+++ public/linkable-ring-signature/main.go
@@ -3,10 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/zbohm/lirisi/client"
+	"syscall/js"
+	_ "syscall/js"
 )
 
+var document js.Value
+
 func main() {
+	document = js.Global().Get("document")
+
 	status, privateKey := client.GeneratePrivateKey("prime256v1", "PEM")
-	_ = status // TODO: handle error
-	fmt.Println(string(privateKey))
+	fmt.Println("client.GeneratePrivateKey(): ", status)
+	SetValue("pk", "innerHTML", string(privateKey))
+	fmt.Println("Print from Go: ", string(privateKey))
+
+	status, publicKey := client.DerivePublicKey(privateKey, "PEM")
+	fmt.Println("client.DerivePublicKey(): ", status)
+	SetValue("sk", "innerHTML", string(publicKey))
+	fmt.Println("Print from Go: ", string(publicKey))
+}
+
+// SetValue sets the value of the specified element.
+func SetValue(elem string, key string, value string) {
+	getElementById(elem).Set(key, value)
+}
+
+// getElementById returns the element with the specified ID.
+func getElementById(elem string) js.Value {
+	return document.Call("getElementById", elem)
 }
