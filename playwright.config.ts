802ad5bb1698241de44e8e961d9cba4abd6e4cc9
mkamsani <mkamsani@posteo.org>
2024-01-21 00:17:56 +0800

    tests: write e2e test, update playwright config
    - e2e test is broken, help needed
    - only test on chromium for now

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -37,15 +37,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -74,4 +74,16 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  webServer: [
+    {
+      command: 'npm run dev',
+      url: 'http://127.0.0.1:3000',
+      reuseExistingServer: !process.env.CI,
+    },
+    { // Change `cd ../backend &&` if your folder is elsewhere.
+      command: 'cd ../backend && go run . --schema simulation --users 100',
+      url: 'http://127.0.0.1:8080',
+      reuseExistingServer: !process.env.CI,
+    }
+  ]
 });
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,39 +1,78 @@
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test("E2E Test", async ({ page }) => {
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.goto("http://localhost:3000/admin");
+  await page.click('button[id="fold-keys"]')
+  console.log("Keys Folded")
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+  ///
+  /// EDIT THIS LINE TO CHANGE THE USER YOU'RE TESTING
+      let number = 1;
+  ///
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
-});
-
-test("Login Test", async ({ page }) => {
-  // Go to the login page
   await page.goto("http://localhost:3000");
+    console.log(`\nSimulating user ${number}...\n`)
+    const email = `user${number}@sentinelvote.tech`;
+    let password = "password";
+    if (number === 1 || number === 2) {
+      password = "Password1!";
+    }
+    // Go to the login page
 
-  // Fill in the email and password fields
-  await page.fill('input[name="email"]', "user1@sentinelvote.tech");
-  await page.fill('input[name="password"]', "Password1!");
+    // Fill in the email and password fields
+    await page.fill('input[name="email"]', email);
+    await page.fill('input[name="password"]', password);
 
-  // Click the login button
-  await page.click('button[type="submit"]');
+    // Click the login button
+    await page.click('button[type="submit"]');
 
-  // Wait for the response and check if the login was successful
-  // This will depend on what happens in your app upon successful login
-  // For example, you might be redirected to a different page
-  await page.waitForNavigation();
-  const url = page.url();
-  expect(url).toBe("http://localhost:3000/voter/pem-uploader");
+    // Wait for the response and check if the login was successful
+    // This will depend on what happens in your app upon successful login
+    // For example, you might be redirected to a different page
+    await page.waitForNavigation();
+    const url = page.url();
+
+    if (url === "http://localhost:3000/voter/pending-election") {
+      console.log("Login Successful");
+    } else if (url === "http://localhost:3000/voter/pem-generate") {
+      console.log("Login Successful");
+    } else if (url !== "http://localhost:3000/voter/pem-uploader") {
+      console.log("Login Failed");
+    } else {
+      console.log("Login Successful");
+      // Our actual test continues here.
+      // Get the cookie user_email
+      const cookie = await page.context().cookies();
+      const email = cookie[0].value;
+      // expect(email).toBe(`user1@sentinelvote.tech`);
+      // Click the login button
+      await page.click('button[id="playwright-skip"]')
+      await page.waitForNavigation();
+      const url = page.url();
+      // expect(url).toBe(`http://localhost:3000/voter/candidate-selection`);
+      console.log("Candidate Selection Page");
+      switch (Math.floor(Math.random()*3) + 1) {
+        case 1:
+          await page.getByLabel("Tharman Shanmugaratnam").check();
+        case 2:
+          await page.getByLabel("Ng Kok Song").check();
+        case 3:
+          await page.getByLabel("Tan Kin Lian").check();
+      }
+
+      // expect(true).toBe(true);
+      await page.click('button[id="submit-vote"]')
+      await page.click('button[id="confirm-vote"]')
+      await page.waitForNavigation();
+      {
+        const url = page.url();
+        // expect(url).toBe(`http://localhost:3000/voter/success`);
+        // click on the only button in the page
+        await page.click('button');
+      }
+      await page.goto("http://localhost:3000");
+    }
 });
