1b0e7aef84631df49914fe8878efa59edd30ae05
mkamsani <mkamsani@posteo.org>
2024-01-30 12:30:42 +0800

    feat: improve blockchain wrappers on next api
    - if fabric is down, /vote returns a blank json of the expected format.
    - if fabric is down, /folded-public-keys returns a string.
    - /folded-public-keys returns plaintext, thus removing json parse step.

--- src/app/api/fabric/folded-public-keys/route.ts
+++ src/app/api/fabric/folded-public-keys/route.ts
@@ -1,43 +1,31 @@
-import {NextRequest, NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { AuthorizationToken } from "@/app/api/fabric/route";
 
 export const revalidate = 0;
 export async function GET(request: NextRequest) {
-
-    let response = await fetch(`${process.env.NEXT_PUBLIC_FABRIC_URL}/user/enroll`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer",
-            "Cache-Control": "no-cache, no-store, must-revalidate", // Prevent caching
-            "Pragma": "no-cache", // For compatibility with HTTP/1.0
-            "Expires": "0" // Proxies
-        },
-        body: JSON.stringify({
-            id: "admin",
-            secret: "adminpw",
-        }),
-    });
-    const { token: authorizationToken } = await response.json();
-
-    let fabricBody = JSON.stringify({
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_FABRIC_URL}/query/vote-channel/SentinelVote`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${await AuthorizationToken()}`,
+        "Cache-Control": "no-cache, no-store, must-revalidate", // Prevent caching
+        "Pragma": "no-cache", // For compatibility with HTTP/1.0
+        "Expires": "0" // Proxies
+      },
+      body: JSON.stringify({
         "method": "KVContractGo:GetFoldedPublicKeys",
         "args":   [],
+      }),
     });
-    response = await fetch(`${process.env.NEXT_PUBLIC_FABRIC_URL}/query/vote-channel/SentinelVote`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${authorizationToken}`,
-            "Cache-Control": "no-cache, no-store, must-revalidate", // Prevent caching
-            "Pragma": "no-cache", // For compatibility with HTTP/1.0
-            "Expires": "0" // Proxies
-        },
-        body: fabricBody
-    });
-
-    const { response: foldedPublicKeys } = await response.json();
-
-    const res = new NextResponse(JSON.stringify(foldedPublicKeys));
-    res.headers.set('Content-Type', 'application/json');
+    const responseJSON = await response.json();
+    const res = new NextResponse(responseJSON.response);
+    res.headers.set('Content-Type', 'text/plain');
     return res;
+  } catch (e) {
+    console.log(e);
+    const res = new NextResponse("Missing/Unset");
+    res.headers.set('Content-Type', 'text/plain');
+    return res;
+  }
 }
--- src/app/api/fabric/vote/route.ts
+++ src/app/api/fabric/vote/route.ts
@@ -1,43 +1,33 @@
-import {NextRequest, NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { AuthorizationToken } from "@/app/api/fabric/route";
 
 export const revalidate = 0;
 export async function GET(request: NextRequest) {
-
-    let response = await fetch(`${process.env.NEXT_PUBLIC_FABRIC_URL}/user/enroll`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer",
-            "Cache-Control": "no-cache, no-store, must-revalidate", // Prevent caching
-            "Pragma": "no-cache", // For compatibility with HTTP/1.0
-            "Expires": "0" // Proxies
-        },
-        body: JSON.stringify({
-            id: "admin",
-            secret: "adminpw",
-        }),
-    });
-    const { token: authorizationToken } = await response.json();
-
-    let fabricBody = JSON.stringify({
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_FABRIC_URL}/query/vote-channel/SentinelVote`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${await AuthorizationToken()}`,
+        "Cache-Control": "no-cache, no-store, must-revalidate", // Prevent caching
+        "Pragma": "no-cache", // For compatibility with HTTP/1.0
+        "Expires": "0" // Proxies
+      },
+      body: JSON.stringify({
         "method": "KVContractGo:GetVotes",
         "args":   [],
+      }),
     });
-    response = await fetch(`${process.env.NEXT_PUBLIC_FABRIC_URL}/query/vote-channel/SentinelVote`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${authorizationToken}`,
-            "Cache-Control": "no-cache, no-store, must-revalidate", // Prevent caching
-            "Pragma": "no-cache", // For compatibility with HTTP/1.0
-            "Expires": "0" // Proxies
-        },
-        body: fabricBody
+    const responseJSON = await response.json();
+    return NextResponse.json(responseJSON.response);
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      "countCandidate": {},
+      "countConstituency": {},
+      "countHour": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      "countTotal": 0,
+      "raw": null
     });
-
-    const { response: votes } = await response.json();
-
-    const res = new NextResponse(JSON.stringify(votes));
-    res.headers.set('Content-Type', 'application/json');
-    return res;
+  }
 }
