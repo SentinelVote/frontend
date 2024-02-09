abebdb4b25674823dbea2b5cff64bbfbb4cd9d9e
mkamsani <mkamsani@posteo.org>
2024-02-09 21:38:28 +0800

    chore: playwright browser must be set in CLI

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,11 +34,11 @@ export default defineConfig({
   timeout: 60000,
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+  // projects: [
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
     // {
     //   name: 'firefox',
@@ -69,5 +69,5 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  // ],
 });
